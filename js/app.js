fb469a3044581ed1d3ec2660637bf93fc62d7614
Bin Lan <binlan@google.com>
2021-04-12 21:52:57 -0400

    Made minus more reliable.

--- js/app.js
+++ js/app.js
@@ -10,10 +10,15 @@ App.IndexRoute = Ember.Route.extend({
     for (var i = 0; i < 8; i++) {
       var line = [];
       for (var j = 0; j < 5; j++) {
+        const seed = 200;
         var item = {};
-        item.first = Math.floor((Math.random()*15));
-        item.second = Math.floor((Math.random()*15));
         item.op = '+';
+        item.first = safeRandom(seed);
+        item.second = safeRandom(seed);
+        if (Math.random() < 0.5) {
+          item.op = '-';
+          item.second = safeRandom(item.first);
+        }
         line.push(item);
       }
       result.push(line);
@@ -21,3 +26,16 @@ App.IndexRoute = Ember.Route.extend({
     return result;
   }
 });
+
+function safeRandom(seed) {
+  var result = betterRandom(seed);
+  while (result == 0 && seed != 1) {
+    result = Math.floor(betterRandom(seed));
+  }
+  return result;
+}
+
+function betterRandom(seed) {
+  return crypto.getRandomValues(new Uint32Array(1))[0] % seed;
+}
+
