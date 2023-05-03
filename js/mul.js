cff52d0680275eb37305f11836041740d22169d3
Bin Lan <binlan@google.com>
2023-05-03 16:02:10 -0400

    Add a set to avoid dup questions

--- js/mul.js
+++ js/mul.js
@@ -6,10 +6,11 @@ App.Router.map(function() {
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
+    var records = new Set();
     var result = [];
     for (var i = 0; i < 8; i++) {
       var line = [];
-      for (var j = 0; j < 5; j++) {
+      for (var j = 0; j < 5; ) {
         const seed = 13;
         var item = {};
         item.op = '*';
@@ -19,7 +20,11 @@ App.IndexRoute = Ember.Route.extend({
           item.op = '';
           item.first = item.first * item.second
         }
-        line.push(item);
+        if (!records.has(item)) {
+          line.push(item);
+          records.add(item);
+          j++;
+        }
       }
       result.push(line);
     }
